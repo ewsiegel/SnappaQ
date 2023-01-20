59372dbfda127643474d787d371b7f83eca0ca13
Brady Klein <bmklein@mit.edu>
2023-01-20 00:19:22 -0500

    updates for game component (state and timestamp)

--- server/models/game.js
+++ server/models/game.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const GameSchema = new mongoose.Schema({
   gameType: String,
   gameId: String,
+  state: String,
   players: {
     team1: [String], // list of player IDs
     team2: [String], // list of player IDs
   },
   winners: [String],
   losers: [String],
+  timestamp: { type: Date, default: null }, // date & time that a game began, set this when game state becomes active
 });
 
 // compile model from schema
