1d1608cb685e84c687e5139c5b0ff4034a5eb7b7
Brady Klein <bmklein@mit.edu>
2023-01-29 20:55:08 -0500

    close button

--- client/src/components/modules/EditItemPopup.js
+++ client/src/components/modules/EditItemPopup.js
@@ -4,7 +4,6 @@ import "./NewItem.css";
 import { post } from "../../utilities";
 import { NewItem } from "../modules/NewItem.js";
 
-
 /**
  * List of users that are online to chat with and all chat
  *
@@ -13,22 +12,32 @@ import { NewItem } from "../modules/NewItem.js";
  * @param {UserObj} userId of logged-in user
  * @param {} active
  * @param {} profiles
+ * @param {} setDisplayEditItem
  */
 
 const EditItemPopup = (props) => {
   return props.trigger ? (
     <div className="u-flex u-relative Popup-container">
-        <div className="Popup-inner">
-            <h3>Edit item</h3>
-            {/* <NewItem
+      <div className="Popup-inner">
+        <h3>Edit item</h3>
+        <button
+          className="close-btn"
+          onClick={() => {
+            props.setDisplayEditItem(false);
+          }}
+        >
+          close
+        </button>
+        {/* <NewItem
                 players={props.data}
                 active={props.active}
                 profiles={props.profiles}
             /> */}
-        </div>
-
+      </div>
     </div>
-  ) : "";
+  ) : (
+    ""
+  );
 };
 
 export default EditItemPopup;
